3028bec6de9fbf2dbbe6686480778303d6c41718
Guilherme Rocha <guilherme.tecnologia1995@gmail.com>
2026-08-07 15:41:20 -0300

    Remove root Terraform files and add vm-windows lab

--- modules/resource_group/main.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "rg-lab-02" {
-  name     = var.resource_group_name
-  location = var.location
-}
--- modules/resource_group/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_name" {
-  description = "Nome do Resource Group"
-  type        = string
-}
-
-variable "location" {
-  description = "Localização dos recursos"
-  type        = string
-}
