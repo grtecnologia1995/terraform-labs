b102e6b3f2f8a9ed06d8b0cbac34ce36d2042263
Guilherme Rocha <guilherme.tecnologia1995@gmail.com>
2026-08-07 15:16:55 -0300

    Lab terraform com modulos

---
+++ modules/resource_group/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  description = "Nome do Resource Group"
+  type        = string
+}
+
+variable "location" {
+  description = "Localização dos recursos"
+  type        = string
+}
